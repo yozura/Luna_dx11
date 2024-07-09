accaa5d912ea58aac4d2f57164c43d0499814cac
Gunho Choi <63341938+yozura@users.noreply.github.com>
2024-07-09 16:03:40 +0900

    #11 [7.13] Lighting Demo  (#12)

---
+++ chapter7/Lighting/Lighting.h
@@ -0,0 +1,82 @@
+#ifndef LIGHTING_H
+#define LIGHTING_H
+
+#include "D3DApp.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Waves.h"
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 Pos;
+	DirectX::XMFLOAT3 Normal;
+};
+
+class Lighting : public D3DApp
+{
+public:
+    Lighting(HINSTANCE hInstance);
+    ~Lighting();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	float GetHillHeight(float x, float z) const;
+	DirectX::XMFLOAT3 GetHillNormal(float x, float z) const;
+	void BuildLandGeometryBuffers();
+	void BuildWaveGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+
+private:
+	ID3D11Buffer* mLandVertexBuffer;
+	ID3D11Buffer* mLandIndexBuffer;
+	
+	ID3D11Buffer* mWavesVertexBuffer;
+	ID3D11Buffer* mWavesIndexBuffer;
+
+	Waves mWaves;
+	DirectionalLight mDirLight;
+	PointLight mPointLight;
+	SpotLight mSpotLight;
+	Material mLandMat;
+	Material mWavesMat;
+
+	ID3DX11Effect* mFX;
+	ID3DX11EffectTechnique* mTech;
+	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+	ID3DX11EffectMatrixVariable* mfxWorld;
+	ID3DX11EffectMatrixVariable* mfxWorldInvTranspose;
+	ID3DX11EffectVectorVariable* mfxEyePosW;
+	ID3DX11EffectVariable* mfxDirLight;
+	ID3DX11EffectVariable* mfxPointLight;
+	ID3DX11EffectVariable* mfxSpotLight;
+	ID3DX11EffectVariable* mfxMaterial;
+
+	ID3D11InputLayout* mInputLayout;
+
+	DirectX::XMFLOAT4X4 mLandWorld;
+	DirectX::XMFLOAT4X4 mWavesWorld;
+	DirectX::XMFLOAT4X4 mView;
+	DirectX::XMFLOAT4X4 mProj;
+
+	UINT mLandIndexCount;
+
+	DirectX::XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+#endif /* LIGHTING_H */
