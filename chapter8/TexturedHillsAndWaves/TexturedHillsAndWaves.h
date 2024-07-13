bcee8ea62fe74f4dc938c587504bf8b7c260c99f
Gunho Choi <63341938+yozura@users.noreply.github.com>
2024-07-13 11:47:11 +0900

    #19 [8.10] Textured Hiils and Waves Demo (#20)

---
+++ chapter8/TexturedHillsAndWaves/TexturedHillsAndWaves.h
@@ -0,0 +1,69 @@
+#ifndef CRATE_H
+#define CRATE_H
+
+#include "D3DApp.h"
+#include "GeometryGenerator.h"
+#include "LightHelper.h"
+#include "MathHelper.h"
+#include "Vertex.h"
+#include "Effects.h"
+#include "Waves.h"
+
+class TexturedHillsAndWaves : public D3DApp
+{
+public:
+	TexturedHillsAndWaves(HINSTANCE hInstance);
+	~TexturedHillsAndWaves();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	float GetHillHeight(float x, float z) const;
+	DirectX::XMFLOAT3 GetHillNormal(float x, float z) const;
+	void BuildLandGeometryBuffers();
+	void BuildWaveGeometryBuffers();
+
+private:
+	ID3D11Buffer* mLandVertexBuffer;
+	ID3D11Buffer* mLandIndexBuffer;
+	
+	ID3D11Buffer* mWavesVertexBuffer;
+	ID3D11Buffer* mWavesIndexBuffer;
+
+	ID3D11ShaderResourceView* mGrassMapSRV;
+	ID3D11ShaderResourceView* mWaterMapSRV;
+
+	Waves mWaves;
+
+	DirectionalLight mDirLights[3];
+	Material mLandMat;
+	Material mWavesMat;
+
+	DirectX::XMFLOAT4X4 mGrassTexTransform;
+	DirectX::XMFLOAT4X4 mWaterTexTransform;
+	DirectX::XMFLOAT4X4 mLandWorld;
+	DirectX::XMFLOAT4X4 mWavesWorld;
+
+	DirectX::XMFLOAT4X4 mView;
+	DirectX::XMFLOAT4X4 mProj;
+
+	UINT mLandIndexCount;
+	DirectX::XMFLOAT2 mWaterTexOffset;
+
+	DirectX::XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+#endif /* CRATE_H */
